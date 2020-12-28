4b8f8ca1e56dcf8671fffc18d07419d9223ea374
normtema <norm.temach@gmail.com>
2020-12-28 15:55:44 +0200

    admin-panel without styles

---
+++ resources/views/admin/post/edit.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.admin_layout')
+
+@section('title', 'Edit article')
+
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Edit article: {{ $post['title'] }}</h1>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+            @if (session('success'))
+                <div class="alert alert-success" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                    <h4><i class="icon fa fa-check"></i>{{ session('success') }}</h4>
+                </div>
+            @endif
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card card-primary">
+                        <!-- form start -->
+                        <form action="{{ route('post.update', $post['id']) }}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Name</label>
+                                    <input type="text" value="{{ $post['title'] }}" name="title" class="form-control"
+                                        id="exampleInputEmail1" placeholder="enter name of article" required>
+                                </div>
+                                <div class="form-group">
+                                    <!-- select -->
+                                    <div class="form-group">
+                                        <label>Choose category:</label>
+                                        <select name="cat_id" class="form-control" required>
+                                            @foreach ($categories as $category)
+                                                <option value="{{ $category['id'] }}" @if ($category['id'] == $post['cat_id']) selected
+                                            @endif>{{ $category['title'] }}
+                                            </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <textarea name="text" class="editor">{{ $post['text'] }}</textarea>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="feature_image">photo of article</label>
+                                    <img src="{{ $post['img'] }}" alt="" class="img-uploaded"
+                                        style="display: block; width: 300px">
+                                    <input type="text" value="{{ $post['img'] }}" name="img" class="form-control"
+                                        id="feature_image" name="feature_image" value="" readonly>
+                                    <a href="" class="popup_selector" data-inputid="feature_image">choose photo</a>
+                                </div>
+                            </div>
+                            <!-- /.card-body -->
+
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary">Save</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+@endsection
